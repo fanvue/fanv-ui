12a234c99a9e092c62c3ff4ee4fee33e347341ff
Samuel Ribeiro <samuel@fanvue.com>
2026-01-26 15:44:15 +0000

    feat: set up project tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({include: ['lib'],
+    tsconfigPath: resolve(__dirname, 'tsconfig.app.json'),
+  })],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+    },
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      name: 'fanv-ui',
+      fileName: 'index',
+    },
+  },
 })
