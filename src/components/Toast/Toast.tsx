b9b996bca28a7770a804905f33ed8b034bbe430f
Samuel Ribeiro <samuel@fanvue.com>
2026-02-05 13:20:01 +0000

    chore: more svg icons

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,6 +1,7 @@
 import * as ToastPrimitive from "@radix-ui/react-toast";
 import * as React from "react";
 import { cn } from "../../utils/cn";
+import { CloseIcon } from "../Icons/Close";
 import { ErrorIcon } from "../Icons/Error";
 import { InfoIcon } from "../Icons/Info";
 import { SuccessIcon } from "../Icons/Success";
@@ -127,15 +128,7 @@ export const Toast = React.forwardRef<React.ComponentRef<typeof ToastPrimitive.R
         {action && <ToastPrimitive.Action altText="Action">{action}</ToastPrimitive.Action>}
         {showClose && (
           <ToastPrimitive.Close className="absolute top-2 right-2 rounded-md p-1 opacity-70 transition-opacity hover:opacity-100 focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-4 w-4"
-              aria-hidden="true"
-            >
-              <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-            </svg>
+            <CloseIcon className="size-4" />
             <span className="sr-only">Close</span>
           </ToastPrimitive.Close>
         )}
