fe6848915b8bd7af1fc4fffaac207ccec77c71fb
Samuel Ribeiro <samuel@fanvue.com>
2026-02-10 15:20:29 +0000

    chore: import icon

--- src/components/IconButton/IconButton.test.tsx
+++ src/components/IconButton/IconButton.test.tsx
@@ -1,19 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { axe } from "vitest-axe";
+import { HomeIcon } from "../Icons";
 import { IconButton } from "./IconButton";
-
-const HomeIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    className="size-5"
-    aria-hidden="true"
-  >
-    <path d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" />
-  </svg>
-);
 
 describe("IconButton", () => {
   describe("API", () => {
